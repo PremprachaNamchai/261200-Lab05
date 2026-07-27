4639aa5c56f3726ef1a99ac6d3546bb7a01fd406
prem <prem0814675274@gmail.com>
2026-07-27 07:18:24 +0700

    Refactor Main class to use LocalDate for birthdays

--- Main.java
+++ Main.java
@@ -1,14 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        User john = new User("John", 1954, 2, 18);
-        john.displayInfo();
+        LocalDate today = LocalDate.now();
 
-        Admin nicolas = new Admin("Nicolas", 1964, 7, 30);
-        nicolas.displayInfo();
-        nicolas.displayInfo(true);
-        nicolas.displayInfo(false);
-        nicolas.displayHappyBirthday();
+        User user = new User("John", today.getYear() - 1, today.getMonthValue(), today.getDayOfMonth());
+        Admin admin = new Admin("Nicolas", today.getYear() - 1, today.getMonthValue(), today.getDayOfMonth());
+
+        System.out.println(user.getBirthdayGreeting());
+        System.out.println(admin.getBirthdayGreeting());
     }
 }
